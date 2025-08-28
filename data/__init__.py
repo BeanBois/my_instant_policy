1231603bf3c02dab207d32252bf119e83dbdd741
kai_thinkpad <k20016655@kcl.ac.uk>
2025-08-28 03:04:58 +0100

    minor fix

--- data/__init__.py
+++ data/__init__.py
@@ -3,6 +3,6 @@ from .pseudo_game import PseudoGame
 
 from .pseudo_configs import SCREEN_HEIGHT as PSEUDO_SCREEN_HEIGHT
 from .pseudo_configs import SCREEN_WIDTH as PSEUDO_SCREEN_WIDTH
-from .game_configs import PLAYER_SIZE,
+from .game_configs import PLAYER_SIZE
 
 from .interface import Game, PlayerState, GameInterface, GameMode, Action
