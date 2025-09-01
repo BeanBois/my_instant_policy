c171a7cb17e9e90e5040039f47c87bb371a4dd2b
kai <kaishun.teoh@gmail.com>
2025-09-01 20:08:38 +0100

    add train script and req.txt for sshgpu

---
+++ train_script.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=<your_username>
+
+WORK_DIR="/vol/bitbucket/kst24/my_instant_policy"
+CUDA_VERSION="11.8.0"
+ENV_NAME="myenv"
+echo "START"
+export PENV=/vol/bitbucket/${USER}/${ENV_NAME}
+
+export PATH=/vol/bitbucket/kst24/fyp/my_instant_policy/:$PATH
+source "/vol/cuda/${CUDA_VERSION}/setup.sh"
+
+
+
+# Install required packages (if not already installed)
+# pip install --user torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+# pip install --user pygame numpy matplotlib
+
+# Set environment variables
+export CUDA_VISIBLE_DEVICES=0
+# python3 -m virtualenv $PENV
+source $PENV/bin/activate
+pip install -r "${WORK_DIR}/requirements.txt"
+
+
+python3 -u "${WORK_DIR}/train.py"
